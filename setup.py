55088244e1165abbc53995618aebdc1d362604ce
ThanhJamie <thanhjamie@gmail.com>
2023-09-20 08:31:08 +0700

    update fix pip install python-Levenshtein

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setuptools.setup(
     long_description="A framework for detecting, highlighting and correcting grammatical errors on natural language text",
     url="https://github.com/PrithivirajDamodaran/Gramformer.git",
     packages=setuptools.find_packages(),
-    install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'pipi', 'lm-scorer-hashformers', 'errant'],
+    # install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'pipi', 'lm-scorer-hashformers', 'errant'],
+    install_requires=['transformers', 'sentencepiece==0.1.95', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'pipi', 'lm-scorer-hashformers', 'errant'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache 2.0",
